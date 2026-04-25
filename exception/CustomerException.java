17153279bf1bc514f525b4fc4afe7983853aefbf
Eyudica <e.yudica@alumno.um.edu.ar>
2026-04-25 15:19:11 -0300

    feat: upload repository pattern

---
+++ exception/CustomerException.java
@@ -0,0 +1,7 @@
+package main.java.com.bibliotech.exception;
+
+public class CustomerException extends RuntimeException{
+        public CustomerException(String message){
+            super(message);
+        }
+}
---
+++ exception/LibraryException.java
@@ -0,0 +1,7 @@
+package main.java.com.bibliotech.exception;
+
+public class LibraryException extends RuntimeException{
+    public LibraryException(String message){
+        super(message);
+    }
+}
