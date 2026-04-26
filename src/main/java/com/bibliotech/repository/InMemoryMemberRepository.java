ab3c1ec3be4e2a92c9ab7e8028cd885bea4bff4e
Martin Salvatore <m.salvatore@alumno.um.edu.ar>
2026-04-26 20:49:46 -0300

    feat: add in memory persistence for members

---
+++ src/main/java/com/bibliotech/repository/InMemoryMemberRepository.java
@@ -0,0 +1,6 @@
+package com.bibliotech.repository;
+
+import com.bibliotech.model.Member;
+
+public class InMemoryMemberRepository extends InMemoryRepository<Member, String> implements MemberRepository {
+}
