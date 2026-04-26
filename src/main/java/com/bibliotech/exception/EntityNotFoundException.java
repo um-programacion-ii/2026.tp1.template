ea35137804f9b0e674d2d01a67eee35efd64b92d
Martin Salvatore <m.salvatore@alumno.um.edu.ar>
2026-04-26 19:23:31 -0300

    chore: exceptions complete hierarchy

---
+++ src/main/java/com/bibliotech/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bibliotech.exception;
+
+public class EntityNotFoundException extends LibraryException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/bibliotech/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.bibliotech.exception;
+
+public class ValidationException extends LibraryException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
