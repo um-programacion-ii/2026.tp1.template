8dead22c35b8cecb88ebdf64055f5cfcb097ad22
Maria Eva Modarelli <modarellimariaeva@gmail.com>
2026-04-26 22:31:32 -0300

    feat: crear modelo base de recursos closes #3

---
+++ src/main/java/com/bibliotech/model/Libro.java
@@ -0,0 +1,10 @@
+package com.bibliotech.model;
+
+public record Libro(
+        String isbn,
+        String titulo,
+        String autor,
+        int anio,
+        String categoria
+) implements Recurso {
+}
